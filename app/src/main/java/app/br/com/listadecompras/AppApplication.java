1436b63ee942bce1744f36200e55bc82fc6bbff4
Tiago Ribeiro <tgwebmaster27@gmail.com>
2022-11-01 10:58:59 -0300

    Iniciando projeto Lista de compras e enviando para github

---
+++ app/src/main/java/app/br/com/listadecompras/AppApplication.java
@@ -0,0 +1,32 @@
+package app.br.com.listadecompras;
+
+import android.app.Application;
+import android.util.Log;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+public class AppApplication extends Application {
+
+    public static final String DB_NAME = "ListaDeCompras.realm";
+    public static final int DB_VERSION = 1;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Realm.init(this);
+
+        RealmConfiguration config = new RealmConfiguration.Builder()
+                .name(DB_NAME)
+                .schemaVersion(DB_VERSION)
+                .allowWritesOnUiThread(true)
+                .build();
+
+        Realm realm = Realm.getInstance(config);
+
+        Log.d("db_log", "onCreate: Realm com sucesso: "+DB_VERSION+" versão "+DB_VERSION);
+
+
+    }
+}
